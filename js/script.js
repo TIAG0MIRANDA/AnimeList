9b8d057af3c2e79ad5f870571b10b4f12ea238ed
Tiago Miranda <t.miranda0249@gmail.com>
2026-03-02 15:23:59 -0300

    Ajustamos o nav

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ const items = document.querySelectorAll(".item");
 const line = document.querySelector(".line");
 
 function moveLine(element) {
-    const line = document.querySelector(".line");
     const nav = document.querySelector(".nav");
-
     const elementRect = element.getBoundingClientRect();
     const navRect = nav.getBoundingClientRect();
 
@@ -12,12 +10,21 @@ function moveLine(element) {
     line.style.left = (elementRect.left - navRect.left) + "px";
 }
 
+function setActive(text) {
+    items.forEach(i => {
+        i.querySelector("span").classList.remove("active");
+    });
+    text.classList.add("active");
+}
+
 items.forEach(item => {
     item.addEventListener("click", () => {
         const text = item.querySelector("span");
+        setActive(text);
         moveLine(text);
     });
 });
 
 const firstText = items[0].querySelector("span");
+setActive(firstText);
 moveLine(firstText);

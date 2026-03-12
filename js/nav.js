4ed8a1c85c8055df0cda101bd64ec8bddae85285
Tiago Miranda <t.miranda0249@gmail.com>
2026-03-12 15:06:56 -0300

    Box - Stylisation

--- js/nav.js
+++ js/nav.js
@@ -4,11 +4,21 @@ const line = document.querySelector(".line");
 const nav = document.querySelector(".nav");
 
 // FUNÇÕES
+
+// FUNÇÃO que move a linha para baixo do item do menu selecionado
 function moveLine(element) {
+
+    // Pega o tamanho e a posição do elemento clicado (ex: um botão do menu)
     const elementRect = element.getBoundingClientRect(); 
+
+    // Pega o tamanho e a posição da navbar inteira
     const navRect = nav.getBoundingClientRect();
 
+    // Define a largura da linha igual à largura do elemento clicado
     line.style.width = elementRect.width + "px";
+
+    // Define a posição horizontal da linha
+    // Calcula a distância do elemento até o início da navbar
     line.style.left = (elementRect.left - navRect.left) + "px";
 }
 
